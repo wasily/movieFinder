977f1470872a2e2a75f86f12fdd8a0dd6085fcf2
was1ly <was1ly@yandex.ru>
2019-10-19 13:49:18 +0300

    movie controller and repository updated

--- src/main/java/ru/otus/movieFinder/controllers/MovieController.java
+++ src/main/java/ru/otus/movieFinder/controllers/MovieController.java
@@ -1,11 +1,9 @@
 package ru.otus.movieFinder.controllers;
 
+import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.otus.movieFinder.model.domain.ContentRelease;
 import ru.otus.movieFinder.model.domain.Movie;
 import ru.otus.movieFinder.model.dto.MovieRequestDTO;
@@ -14,23 +12,27 @@ import ru.otus.movieFinder.services.ReleasesService;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequiredArgsConstructor
 public class MovieController {
     private final MovieService movieService;
     private final ReleasesService releasesService;
 
-    @GetMapping("/movies")
+    @PostMapping("/movies")
+    @ApiOperation(value = "Получить список фильмов по части названия")
     public ResponseEntity<List<Movie>> listMoviesByTitle(@RequestBody MovieRequestDTO movie) {
         return ResponseEntity.ok().body(movieService.getMoviesByTitle(movie.getTitle()));
     }
 
     @GetMapping("/movies/{imdbId}")
-    public ResponseEntity<Movie> getMovieByImdbId(@PathVariable String imdbId) {
+    @ApiOperation(value = "Получить фильм по idmbId")
+    public ResponseEntity<Movie> getMovieByImdbId(@PathVariable @ApiParam(value = "imdbId", example = "tt9243946") String imdbId) {
         return ResponseEntity.ok().body(movieService.getMovieByImdbId(imdbId));
     }
 
-    @GetMapping("/releases/movies")
+    @PostMapping("/releases/movies")
+    @ApiOperation(value = "Получить список релизов фильмов по части названия")
     public ResponseEntity<List<ContentRelease>> listMovieReleasesByTitle(@RequestBody MovieRequestDTO movie) {
         return ResponseEntity.ok().body(releasesService.getMovieReleases(movie.getTitle()));
     }
--- src/main/java/ru/otus/movieFinder/repositories/RutrackerRepository.java
+++ src/main/java/ru/otus/movieFinder/repositories/RutrackerRepository.java
@@ -7,6 +7,6 @@ import ru.otus.movieFinder.model.domain.ContentRelease;
 import java.util.List;
 
 public interface RutrackerRepository extends MongoRepository<ContentRelease, String> {
-    @Query("{ 'title' : {$regex:?0, $options:'i'}, 'contentType' : ?1 }")
+    @Query(value = "{ 'title' : {$regex:?0, $options:'i'}, 'contentType' : ?1 }", sort = "{\"regTime\" : -1}")
     List<ContentRelease> findByTitleContaining(String title, String contentType);
 }
